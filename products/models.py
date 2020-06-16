78ee79b3dbd1fa011ace31ebf8cee61d8d3d30af
Irina <iratushina17@gmail.com>
2020-06-16 22:52:30 +0000

    Product and Category models added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        name = models.CharField(max_length=254)
+        friendly_name = models.CharField(max_length=254,
+                                         null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2,
+                                 null=True, blank=True)
+    weight = models.IntegerField(null=True, blank=True)
+    image1 = models.ImageField(null=True, blank=True)
+    image2 = models.ImageField(null=True, blank=True)
+    image3 = models.ImageField(null=True, blank=True)
+    in_stock = models.Boolean(default=False)
+
+    def __str__(self):
+        return self.name
