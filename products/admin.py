db4d1d42626523f14fa4be43e4078283fde955cd
Irina <iratushina17@gmail.com>
2020-06-17 16:30:41 +0000

    Refactor product model, ImageGallery model created, products dada updated

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, ImageGallery
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'has_weight',
+        'image_gallery',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ImageGalleryAdmin(admin.ModelAdmin):
+   list_display = (
+        'friendly_name',
+        'name',
+        'image',
+        'image_url',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ImageGallery, ImageGalleryAdmin)
--- products/models.py
+++ products/models.py
@@ -16,9 +16,28 @@ class Category(models.Model):
         return self.friendly_name
 
 
+class ImageGallery(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Image Galleries'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
+    image_gallery = models.ForeignKey('ImageGallery', null=True, blank=True,
+                                      on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField(max_length=800)
@@ -26,12 +45,6 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2,
                                  null=True, blank=True)
     has_weight = models.BooleanField(default=False, null=True, blank=True)
-    image1 = models.ImageField(null=True, blank=True)
-    image2 = models.ImageField(null=True, blank=True)
-    image3 = models.ImageField(null=True, blank=True)
-    image_url1 = models.URLField(max_length=1024, null=True, blank=True)
-    image_url2 = models.URLField(max_length=1024, null=True, blank=True)
-    image_url3 = models.URLField(max_length=1024, null=True, blank=True)
     in_stock = models.BooleanField(default=False)
 
     def __str__(self):
