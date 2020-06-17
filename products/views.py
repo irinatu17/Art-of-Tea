1853ac1f249ebe55cbcfab083e5bf62805a7f43c
Irina <iratushina17@gmail.com>
2020-06-17 17:07:34 +0000

    View for displaying all products created

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """ A view to display all of the products"""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/all_products.html', context)
