27287cf2db10badb004415baf8feb519260290f6
Irina <iratushina17@gmail.com>
2020-06-21 20:27:41 +0000

    Updated settings for using crispy forms fot automatic bootstrap styling

--- art_of_tea/settings.py
+++ art_of_tea/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'products',
     'about',
     'contact',
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'art_of_tea.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,6 +84,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
