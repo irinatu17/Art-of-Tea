5a1f1c589b8daacf60636211f99e8226214fd2b6
Irina <iratushina17@gmail.com>
2020-06-22 16:54:57 +0000

    Google Map with a marker added

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    //Function to initialize Map 
+    function initMap() {
+        const artOfTea = {lat: 53.349744, lng: -6.260177};
+        const map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: artOfTea});
+        const marker = new google.maps.Marker({position: artOfTea, map: map});
+    };
     //Hiding search field on mobile when the page loads
    $('#search-form-container').hide();
 
@@ -6,4 +12,7 @@ $(document).ready(function() {
     $("#search-button").click(function(){
     $("#search-form-container").toggle();
   });
+
+initMap();
+
 });
