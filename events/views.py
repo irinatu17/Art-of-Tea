83c7b6db06f05ab7237ce9fed65cae5bfa025cde
Irina <iratushina17@gmail.com>
2020-06-22 18:15:58 +0000

    Events view added to render events page

--- events/views.py
+++ events/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def events(request):
+    """
+    A view to return events page
+    """
+    return render(request, 'events/events.html')
