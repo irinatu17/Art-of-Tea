c93e5feff3835f4e3382d7ef0a3be0710dbeebf3
Irina <iratushina17@gmail.com>
2020-06-30 14:58:32 +0000

    Added radios form to the product details template

--- art_of_tea/settings.py
+++ art_of_tea/settings.py
@@ -124,8 +124,6 @@ WSGI_APPLICATION = 'art_of_tea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
-
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
