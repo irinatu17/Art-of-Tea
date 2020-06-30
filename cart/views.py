0c189ec0ede8a673e88295ef7a5e3a63651ed340
Irina <iratushina17@gmail.com>
2020-06-30 18:19:39 +0000

    Fxed the name of the view in cart app

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-def view_bag(request):
+def view_cart(request):
     """ A view that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
