f59c69b25ebd2d29a5f506d8527c7adf3a905182
Irina <iratushina17@gmail.com>
2020-06-30 19:01:47 +0000

    Created contexts file and added it to the list of context processors in settings

--- art_of_tea/settings.py
+++ art_of_tea/settings.py
@@ -87,6 +87,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 def view_cart(request):
     """ A view that renders the cart contents page """
 
