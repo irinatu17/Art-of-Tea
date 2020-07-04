c359998d4c126f9d5c9169341c097441c23a2464
Irina <iratushina17@gmail.com>
2020-07-04 16:33:29 +0000

    Added checkout url, template created

--- art_of_tea/urls.py
+++ art_of_tea/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('events/', include('events.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
