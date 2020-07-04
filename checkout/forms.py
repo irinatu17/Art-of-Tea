2e265dce40e56f4b1e81c4413ea66c7b06537f56
Irina <iratushina17@gmail.com>
2020-07-04 17:42:08 +0000

    Removed asterisk from the required fields in the Order form

--- checkout/forms.py
+++ checkout/forms.py
@@ -12,25 +12,21 @@ class OrderForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         """
-        Add placeholders, remove auto-generated
-        labels, add asterisk to the required fields
+        Add placeholders, remove auto-generated labels
         """
         super().__init__(*args, **kwargs)
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email',
             'phone_number': 'Phone Number',
-            'town_or_city': 'Town or City',
             'address_line1': 'Address Line 1',
             'address_line2': 'Address Line 2',
-            'county': 'County',
-            'postcode': 'Postal Code',
+            'town_or_city': 'Town/City',
+            'county': 'County/State',
+            'postcode': 'Postcode',
             'country': 'Country',
         }
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
+            placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].label = False
