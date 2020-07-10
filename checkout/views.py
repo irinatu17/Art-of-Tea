8b4ef1def652bb895d63eb65fe87b76907510e97
Irina <iratushina17@gmail.com>
2020-07-10 22:14:30 +0000

    Payment intent created

--- checkout/views.py
+++ checkout/views.py
@@ -3,21 +3,37 @@ from django.conf import settings
 from django.contrib import messages
 
 from .forms import OrderForm
+from cart.contexts import cart_contents
+
+import stripe
 
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     cart = request.session.get('cart', {})
     if not cart:
-        messages.error(request, "Your cart is empty at the moment")
+        messages.error(request, "There's nothing in your cart at the moment")
         return redirect(reverse('products'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+    if not stripe_public_key:
+        messages.error(request, 'Stripe public key is missing.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'stripe_client_secret': 'test client secret',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
