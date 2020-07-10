f89dcea2ed46afa9c6efedc57b951dedc84723cf
Irina <iratushina17@gmail.com>
2020-07-10 21:32:51 +0000

    Error handler added from stripe documentation

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,11 +1,12 @@
 /*
-    Core logic/payment flow for this comes from here:
+    Core logic/payment flow for this comes from Stripe documentations:
     https://stripe.com/docs/payments/accept-a-payment
+    with minor modification and additions
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -22,5 +23,23 @@ var style = {
         iconColor: '#dc3545'
     }
 };
+// Create an instance of the card Element
 var card = elements.create('card', {style: style});
+// Add an instance of the card Element into the `card-element` <div>
 card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-exclamation-circle"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
