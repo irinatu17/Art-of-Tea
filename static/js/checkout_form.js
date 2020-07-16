6535a8fb5b8166c63942387b175d9476ec7fe00d
Irina <iratushina17@gmail.com>
2020-07-16 16:24:09 +0000

    Added save-info field

--- static/js/checkout_form.js
+++ static/js/checkout_form.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
 const personalDetailsForm = document.querySelector('#personal-details-form');
 const submitButton = document.getElementById('personal-details-btn');
 let personalDetailsFormValid;
+ var $tabs = $('#nav-tab li');
+
 
 submitButton.addEventListener('click', () => {
     personalDetailsFormValid = personalDetailsForm.checkValidity();
     
     if (personalDetailsFormValid) {
-        $('.nav-tabs .active').parent().next('li').find('a').trigger('click');
+        //$('.nav-tabs .active').parent().next('li').find('a').trigger('click');
         let name = document.getElementById('id_full_name').value; 
         let email = document.getElementById('id_email').value;  
         let phoneNumber = document.getElementById('id_phone_number').value; 
@@ -16,6 +18,18 @@ submitButton.addEventListener('click', () => {
         let nameInput = document.getElementById('full_name').value = name;
         let emailInput = document.getElementById('email').value = email;
         let phoneNumberInput = document.getElementById('phone_number').value = phoneNumber;
+        function showPrev() {
+            $('.nav-tabs .active').parent().next('li').removeClass("disabled");
+            $('.nav-tabs .active').parent().next('li').find('a[data-toggle]').each(function () {
+            $(this).attr("data-toggle", "tab");
+            });
+
+            $('.nav-tabs .active').parent().next('li').find('a[data-toggle="tab"]').tab('show');
+
+            $tabs.filter('.active').next('li').find('a[data-toggle="tab"]').each(function () {
+                $(this).attr("data-toggle", "").parent('li').addClass("disabled");        
+            })
+        }
     }
 });
 
@@ -36,7 +50,12 @@ submitButton2.addEventListener('click', () => {
         let county =  document.getElementById('id_county').value; 
         let postcode =  document.getElementById('id_postcode').value; 
         let country =  document.getElementById('id_country').value; 
+        let saveInfo = Boolean($('#id-save-info'));
+
+       // let saveInfo = document.getElementById('id-save-info').value; 
+       
         $('.nav-tabs .active').parent().next('li').find('a').trigger('click');
+
 
         let addressLine1Input = document.getElementById('address_line1').value = addressLine1;
         let addressLine2Input = document.getElementById('address_line2').value = addressLine2;
@@ -44,6 +63,10 @@ submitButton2.addEventListener('click', () => {
         let countyInput = document.getElementById('county').value = county;
         let postcodeInput = document.getElementById('postcode').value = postcode;
         let countryInput = document.getElementById('country').value = country;
+        let saveInfoInput = document.getElementById('save_info').value = saveInfo;
+
+        console.log(saveInfoInput)
+
     }
 });
 // add the values to hidden inputs in the final form and submit 
