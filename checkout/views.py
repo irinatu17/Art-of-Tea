049451712e5ffa6f3d0e03fd076ecc10c3f6e72a
Irina <iratushina17@gmail.com>
2020-07-16 21:47:36 +0000

    Cache_checkout_data view created for storing save-info checkbox input

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, \
+    reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.conf import settings
 from django.contrib import messages
 
@@ -9,6 +11,7 @@ from products.models import Product
 from cart.contexts import cart_contents
 
 import stripe
+import json
 
 
 def checkout(request):
@@ -104,3 +107,23 @@ def checkout_success(request, order_number):
     }
 
     return render(request, template, context)
+
+
+@require_POST
+def cache_checkout_data(request):
+    """
+    A view to store user's input for the checkbox "save-info" field
+    """
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
