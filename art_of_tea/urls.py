2d086cd241164ee0479e42f1451c9d8c1aa46e69
Irina <iratushina17@gmail.com>
2020-07-17 22:21:58 +0000

    Profile URL added

--- art_of_tea/urls.py
+++ art_of_tea/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
