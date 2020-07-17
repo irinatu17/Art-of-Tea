9694b83586283ac2f6abfa008ddb1d3a49b0b543
Irina <iratushina17@gmail.com>
2020-07-17 22:11:27 +0000

    Created Profile model, updated Order model with profile field; migrations made

--- checkout/models.py
+++ checkout/models.py
@@ -7,10 +7,13 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import Profile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=70, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
