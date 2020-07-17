6d3c13f9f39eb4840dcae6f01c0538fa29a27b04
Irina <iratushina17@gmail.com>
2020-07-17 13:19:30 +0000

    Comleted checkout_form logic, explanatory comments added

--- static/js/checkout_form.js
+++ static/js/checkout_form.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
-    // Personal Details form, step 1
+
+// Personal Details form, step 1
 const personalDetailsForm = document.querySelector('#personal-details-form');
 const submitButton = document.getElementById('personal-details-btn');
 let personalDetailsFormValid;
 
-
+// handles the validation of the first(Personal details) step of the form when "Next" button is clicked
 submitButton.addEventListener('click', () => {
     personalDetailsFormValid = personalDetailsForm.checkValidity();
     
     if (personalDetailsFormValid) {
+        //if valid,  gets the values of the first fieldset
         let name = document.getElementById('id_full_name').value; 
         let email = document.getElementById('id_email').value;  
         let phoneNumber = document.getElementById('id_phone_number').value; 
 
+        // store those values and add them to hidden inputs in the final form to submit
         let nameInput = document.getElementById('full_name').value = name;
         let emailInput = document.getElementById('email').value = email;
         let phoneNumberInput = document.getElementById('phone_number').value = phoneNumber;
 
+        // activate the next tab on click, when form-set is valid 
         $('.nav-tabs .active').parent().next('li').removeClass("disabled");
         $('.nav-tabs .active').parent().next('li').find('a[data-toggle]').attr("data-toggle", "tab");
         $('.nav-tabs .active').parent().next('li').find('a').trigger('click');
@@ -28,12 +32,12 @@ const shippingInfoForm = document.querySelector('#shipping-info-form');
 const submitButton2 = document.getElementById('shipping-info-btn');
 let shippingInfoFormValid;
 
+// handles the validation of the second(Shipping Info) step of the form when "Next" button is clicked
 submitButton2.addEventListener('click', () => {
     let shippingInfoFormValid = shippingInfoForm.checkValidity();
 
     if (shippingInfoFormValid) {
-
-        // grab input values and store in variables
+        //if valid,  gets the values of the second fieldset
         let addressLine1 =  document.getElementById('id_address_line1').value;
         let addressLine2 =  document.getElementById('id_address_line2').value; 
         let townOrCity =  document.getElementById('id_town_or_city').value;
@@ -41,9 +45,7 @@ submitButton2.addEventListener('click', () => {
         let postcode =  document.getElementById('id_postcode').value; 
         let country =  document.getElementById('id_country').value; 
 
-
-        //let saveInfo = document.getElementById('id-save-info').value; 
-
+        // store those values and add them to hidden inputs in the final form to submit
         let addressLine1Input = document.getElementById('address_line1').value = addressLine1;
         let addressLine2Input = document.getElementById('address_line2').value = addressLine2;
         let townOrCityInput = document.getElementById('town_or_city').value = townOrCity;
@@ -51,19 +53,19 @@ submitButton2.addEventListener('click', () => {
         let postcodeInput = document.getElementById('postcode').value = postcode;
         let countryInput = document.getElementById('country').value = country;
 
-        // get save-info input(true or false)
-        let saveInfo = document.getElementById('id-save-info').checked;
-        let saveInfoInput = document.getElementById('save_info').value = saveInfo;  
-        
+        // get save-info value(true or false) if the user is authenticated
+        let saveInfoExist = document.getElementById('id-save-info');
+        if (saveInfoExist){
+            let saveInfo = document.getElementById('id-save-info').checked;
+            let saveInfoInput = document.getElementById('save_info').value = saveInfo;  
+        }
+
+        // activate the next tab on click, when form-set is valid
         $('.nav-tabs .active').parent().next('li').removeClass("disabled");
         $('.nav-tabs .active').parent().next('li').find('a[data-toggle]').attr("data-toggle", "tab");
         $('.nav-tabs .active').parent().next('li').find('a').trigger('click');
-
     }
 });
-// add the values to hidden inputs in the final form and submit 
-
-
 
 // The method of triggering Next and GoBack buttons is taken and modified from the following source:
 // https://stackoverflow.com/questions/22297964/bootstrap-tabs-next-previous-buttons-for-forms/22298275
