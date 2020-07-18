74e97458d636f28623d027f74eba3402087cb838
Irina <iratushina17@gmail.com>
2020-07-18 22:04:07 +0000

    Profile form added

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Profile
+from .forms import ProfileForm
 
 
 def profile(request):
     """ A view to display the user's profile. """
     profile = get_object_or_404(Profile, user=request.user)
+    profile_form = ProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     context = {
-        'profile': profile,
+        'profile_form': profile_form,
+        'orders': orders,
     }
     template = 'profiles/profile.html'
 
