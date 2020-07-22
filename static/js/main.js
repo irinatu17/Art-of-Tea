08fc79fdae832f47642f5f9d0ccd572648bbfe33
Irina <iratushina17@gmail.com>
2020-07-22 21:53:32 +0000

    Refactored contact templates

--- static/js/main.js
+++ static/js/main.js
@@ -1,46 +1,12 @@
 $(document).ready(function() {
 
-
-    //Function to initialize Map 
-    function initMap() {
-        const artOfTea = {lat: 53.349744, lng: -6.260177};
-        const map = new google.maps.Map(document.getElementById('map'), {zoom: 13, center: artOfTea});
-
-        const contentMarker = '<div class="m-2">'+
-          '<h5 class="landing-about-heading">Art of Tea<i class="fab fa-pagelines icon-green ml-2"></i></h5>'+
-          '<h6>Opening hours:</h6>' +
-          '<table>' +
-          '<tr><th>Monday</th><td>10am - 8pm</td></tr>' +
-          '<tr><th>Tuesday</th><td>10am - 8pm</td></tr>' +
-          '<tr><th>Wednesday</th><td>10am - 8pm</td></tr>' +
-          '<tr><th>Thursday</th><td>10am - 8pm</td></tr>' +
-          '<tr><th>Friday</th><td>10am - 10pm</td></tr>' +
-          '<tr><th>Saturday</th><td>11am - 10pm</td></tr>' +
-          '<tr><th>Sunday</th><td>11am - 9pm</td></tr>' +
-          '</table>'
-          +'</div>';
-
-        const infowindow = new google.maps.InfoWindow({
-            content: contentMarker
-        });
-
-        const marker = new google.maps.Marker({
-            position: artOfTea,
-            map: map,
-            title: 'Art of Tea'
-        });
-
-        marker.addListener('click', function() {
-            infowindow.open(map, marker);
-        });
-    };
     //Hiding search field on mobile when the page loads
-   $('#search-form-container').hide();
+    $('#search-form-container').hide();
 
     //Sliding down and up the search form, when the search icon is clicked
     $("#search-button").click(function(){
-    $("#search-form-container").toggle();
-  });
+        $("#search-form-container").toggle();
+    });
 
     //Tempus Dominus bootstrap-datetimepicker
     $(function () {
@@ -49,9 +15,7 @@ $(document).ready(function() {
         });
     });
 
+    // Initialize Bootstrap toasts
+    $('.toast').toast('show');
 
-// Initialize Bootstrap toasts
-  $('.toast').toast('show');
-
-  initMap();
 });
