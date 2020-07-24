2cf1dfbd97af2e1e0a4f9c9d1aab38c2a9022a30
Irina <iratushina17@gmail.com>
2020-07-24 21:36:16 +0000

    Updated Product form, created add_product template

--- products/forms.py
+++ products/forms.py
@@ -6,11 +6,28 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ('name', 'description', 'sku',
+                  'category', 'price',
+                  'rating', 'has_weight',
+                  )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        labels = {
+            'name': 'Product Name',
+            'description': 'Description',
+            'sku': 'SKU',
+            'category': 'Category',
+            'price': 'Price',
+            'rating': 'Rating',
+            'has_weight': 'Product Type',
+        }
+
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-
         self.fields['category'].choices = friendly_names
+        for field in self.fields:
+            self.fields[field].label = labels[field]
+
+        self.fields['description'].widget.attrs['rows'] = 4
+
