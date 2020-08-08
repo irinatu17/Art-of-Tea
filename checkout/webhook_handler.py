72246bb27f71f71ab017b70a38c5d244116e666c
Irina <iratushina17@gmail.com>
2020-08-08 08:01:42 +0000

    Fixed the bug with handling profile in webhook_handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -83,7 +83,6 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
-                    profile=profile,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
@@ -112,6 +111,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
