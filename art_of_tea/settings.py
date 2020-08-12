ec9530d0deb7ae524bc0c5c5372dd5472a5acee7
Irina <iratushina17@gmail.com>
2020-08-12 20:30:49 +0000

    Added TIME_ZONE settings to get correct time for Ireland timezone

--- art_of_tea/settings.py
+++ art_of_tea/settings.py
@@ -236,3 +236,5 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+TIME_ZONE = 'Europe/Dublin'
