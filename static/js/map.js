88663ebb26ebd940b0141e608fc3160e98ef0b81
Irina <iratushina17@gmail.com>
2020-08-18 22:26:05 +0000

    Fixed initMap js function, moved info-window content to template

--- static/js/map.js
+++ static/js/map.js
@@ -1,36 +1,19 @@
+//Function to initialize Map 
+function initMap() {
+    const artOfTea = {lat: 53.349744, lng: -6.260177};
+    const map = new google.maps.Map(document.getElementById('map'), {zoom: 13, center: artOfTea});
 
-    //Function to initialize Map 
-    function initMap() {
-        const artOfTea = {lat: 53.349744, lng: -6.260177};
-        const map = new google.maps.Map(document.getElementById('map'), {zoom: 13, center: artOfTea});
+    const infowindow = new google.maps.InfoWindow({
+        content: document.getElementById('info-window-content')
+    });
 
-        const contentMarker = '<div class="m-2">'+
-          '<h5 class="landing-about-heading">Art of Tea<i class="fab fa-pagelines icon-green ml-2"></i></h5>'+
-          '<h6>Opening hours:</h6>' +
-          '<table>' +
-          '<tr><th>Monday</th><td>10am - 8pm</td></tr>' +
-          '<tr><th>Tuesday</th><td>10am - 8pm</td></tr>' +
-          '<tr><th>Wednesday</th><td>10am - 8pm</td></tr>' +
-          '<tr><th>Thursday</th><td>10am - 8pm</td></tr>' +
-          '<tr><th>Friday</th><td>10am - 10pm</td></tr>' +
-          '<tr><th>Saturday</th><td>11am - 10pm</td></tr>' +
-          '<tr><th>Sunday</th><td>11am - 9pm</td></tr>' +
-          '</table>'
-          +'</div>';
+    const marker = new google.maps.Marker({
+        position: artOfTea,
+        map: map,
+        title: 'Art of Tea'
+    });
 
-        const infowindow = new google.maps.InfoWindow({
-            content: contentMarker
-        });
-
-        const marker = new google.maps.Marker({
-            position: artOfTea,
-            map: map,
-            title: 'Art of Tea'
-        });
-
-        marker.addListener('click', function() {
-            infowindow.open(map, marker);
-        });
-    };
-
-  initMap();
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+};
