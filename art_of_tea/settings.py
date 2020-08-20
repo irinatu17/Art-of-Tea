05d0652f7974fe5b7fafae24d84bd63112f7ed77
Irina <iratushina17@gmail.com>
2020-08-20 20:48:13 +0000

    Attempt 2 to fix image field in Heroku(comment out extra code)

--- art_of_tea/settings.py
+++ art_of_tea/settings.py
@@ -206,14 +206,13 @@ if 'USE_AWS' in os.environ:
     AWS_S3_FILE_OVERWRITE = False
     
     # Static and media files
-    STATIC_URL = "/staticfiles/"
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    # STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
