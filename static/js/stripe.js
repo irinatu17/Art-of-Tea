a5fd178991b1781d8415d48fd62ca298a33763e6
Irina <iratushina17@gmail.com>
2020-08-21 22:44:51 +0000

    Stripe.js refactored, beautofied, optimized

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,21 +1,21 @@
-/*
-    Core logic/payment flow for this comes from Stripe documentations:
-    https://stripe.com/docs/payments/accept-a-payment
-    with minor modification and additions, based on Code Institute mini-project material
-*/
+/**
+ *  Core logic/payment flow for this comes from Stripe documentations:
+ *  https://stripe.com/docs/payments/accept-a-payment
+ *  with minor modification and additions
+ */
 
-var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripePublicKey);
-var elements = stripe.elements();
-var style = {
+const stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+const clientSecret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripePublicKey);
+let elements = stripe.elements();
+const style = {
     base: {
         color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontFamily: '"Open Sans", sans-serif',
         fontSmoothing: 'antialiased',
         fontSize: '16px',
         '::placeholder': {
-            color: '#aab7c4'
+            color: '#495057'
         }
     },
     invalid: {
@@ -24,15 +24,14 @@ var style = {
     }
 };
 // Create an instance of the card Element
-var card = elements.create('card', {style: style});
-// Add an instance of the card Element into the `card-element` <div>
+let card = elements.create('card', {style: style});
 card.mount('#card-element');
 
 // Handle real-time validation errors from the card Element.
 card.addEventListener('change', function (event) {
-    var errorDiv = document.getElementById('card-errors');
+    const errorDiv = document.getElementById('card-errors');
     if (event.error) {
-        var html = `
+        let html = `
             <span class="icon" role="alert">
                 <i class="fas fa-exclamation-circle"></i>
             </span>
@@ -45,7 +44,7 @@ card.addEventListener('change', function (event) {
 });
 
 // Handle form submit
-var form = document.getElementById('payment-form');
+const form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
@@ -53,16 +52,16 @@ form.addEventListener('submit', function(ev) {
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(1200);
     $('.spinner-overlay-wrapper').fadeToggle(1200);
-    var comment = document.getElementById('id_comment').value;
-    var saveInfo = document.getElementById('save_info').value;
-    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
-    var postData = {
+    let comment = document.getElementById('id_comment').value;
+    let saveInfo = document.getElementById('save_info').value;
+    let csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    let postData = {
         'csrfmiddlewaretoken': csrfToken,
         'client_secret': clientSecret,
         'save_info': saveInfo,
         'comment': comment,
     };
-    var url = '/checkout/cache_checkout_data/';
+    let url = '/checkout/cache_checkout_data/';
 
     $.post(url, postData).done(function () {
         stripe.confirmCardPayment(clientSecret, {
@@ -95,8 +94,8 @@ form.addEventListener('submit', function(ev) {
         },
     }).then(function(result) {
             if (result.error) {
-                var errorDiv = document.getElementById('card-errors');
-                var html = `
+                const errorDiv = document.getElementById('card-errors');
+                let html = `
                     <span class="icon" role="alert">
                         <i class="fas fa-exclamation-circle"></i>
                     </span>
