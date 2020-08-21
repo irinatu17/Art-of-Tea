103fedcbbe2ebfdad16ada03a7aad7885ce73870
Irina <iratushina17@gmail.com>
2020-08-21 18:57:27 +0000

    Refactor and optimized map.js code

--- static/js/map.js
+++ static/js/map.js
@@ -1,19 +1,21 @@
-//Function to initialize Map 
+/**
+   * Initilizes the map
+   * from Google Maps API
+   */
 function initMap() {
-    const artOfTea = {lat: 53.349744, lng: -6.260177};
-    const map = new google.maps.Map(document.getElementById('map'), {zoom: 13, center: artOfTea});
-
-    const infowindow = new google.maps.InfoWindow({
-        content: document.getElementById('info-window-content')
-    });
-
-    const marker = new google.maps.Marker({
-        position: artOfTea,
-        map: map,
-        title: 'Art of Tea'
-    });
-
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
+  const mapRef = document.getElementById('map');
+  const contentRef = document.getElementById('info-window-content');
+  const artOfTeaLocation = { lat: 53.349744, lng: -6.260177 };
+  const map = new google.maps.Map(mapRef, { zoom: 13, center: artOfTeaLocation });
+  const infowindow = new google.maps.InfoWindow({
+    content: contentRef,
+  });
+  const marker = new google.maps.Marker({
+    position: artOfTeaLocation,
+    map: map,
+    title: 'Art of Tea',
+  });
+  marker.addListener('click', () => {
+    infowindow.open(map, marker);
+  });
 };
