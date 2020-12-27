ce88ecb69ae7f071190769143c5eec710fe239a0
irinatu17 <ira_tushina@mail.ru>
2020-12-27 22:09:42 +0000

    Navbar dimentions minor adjastments

--- static/js/main.js
+++ static/js/main.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
 });
 
   bttbutton = document.getElementById("back-to-top-btn");
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 30px from the top of the document, show the button
   window.onscroll = function() {scrollFunction()};
   function scrollFunction() {
     if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
