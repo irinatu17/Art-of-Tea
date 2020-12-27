98e10bfbf6032babe4388630a4d0847f914f70c2
irinatu17 <ira_tushina@mail.ru>
2020-12-27 20:54:35 +0000

    Back to top button added

--- art_of_tea/settings.py
+++ art_of_tea/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'art-of-tea.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'art-of-tea.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
--- static/js/main.js
+++ static/js/main.js
@@ -28,4 +28,28 @@ $(document).ready(function () {
    */
   $('.toast').toast('show');
 
+  /**
+   *  Back to Top button
+   */
+
+  $('.btt-link').click(function(e) {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+  })
 });
+
+  bttbutton = document.getElementById("back-to-top-btn");
+// When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+      bttbutton.style.display = "block";
+    } else {
+      bttbutton.style.display = "none";
+    }
+  }
+
+ 
